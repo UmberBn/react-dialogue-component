13f93894ac2388efc518ef71e9adac4e2c1a713c
UmberBn <humberto-bandeira@hotmail.com>
2022-03-07 22:44:53 -0400

    feat: add overlay component

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Overlay = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-color: #000000;
+`;
+
+export const Content = styled.div``;
