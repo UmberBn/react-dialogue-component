b082dd6ed09df3476ff187a9f7b6a0a53aaf76c9
UmberBn <humberto-bandeira@hotmail.com>
2022-03-07 22:59:42 -0400

    feat: add content component

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+const OverlayHeigth = 82;
+
 export const Container = styled.div``;
 
 export const Overlay = styled.div`
@@ -7,6 +9,15 @@ export const Overlay = styled.div`
   height: 100%;
   width: 100%;
   background-color: #000000;
+  min-width: ${OverlayHeigth}px;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: calc(100vh - ${OverlayHeigth}px);
+  width: 100vw;
+  background-color: #FFFFFF;
+  position: fixed;
+  bottom: 0;
+`;
